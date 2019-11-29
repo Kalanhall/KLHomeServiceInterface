81c31cbec2fb8b5a8cee09cfea4c7e2b3646f859
Kimee <wujm002@galanz.com>
2019-11-29 12:43:07 +0800

    init.

---
+++ Example/Pods/Target Support Files/Pods-KLHomeServiceInterface_Example/Pods-KLHomeServiceInterface_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KLHomeServiceInterface_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KLHomeServiceInterface_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-KLHomeServiceInterface_Tests/Pods-KLHomeServiceInterface_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KLHomeServiceInterface_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_KLHomeServiceInterface_Tests
+@end
